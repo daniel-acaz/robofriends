d8de2356edbe438dabb0d7168cc78d008e5e4255
Daniel <daniel.acaz@becommerce.com.br>
2019-07-02 19:12:09 -0300

    Class 66

---
+++ src/robots.js
@@ -0,0 +1,52 @@
+export const robots = [
+  {
+    "id": 1,
+    "name": "Alice",
+    "email": "teste2@gmail.com"
+  },
+  {
+    "id": 2,
+    "name": "Sophia",
+    "email": "teste1@gmail.com"
+  },
+  {
+    "id": 3,
+    "name": "Helena",
+    "email": "teste2@gmail.com"
+  },
+  {
+    "id": 4,
+    "name": "Valentina",
+    "email": "teste1@gmail.com"
+  },
+  {
+    "id": 5,
+    "name": "Laura",
+    "email": "teste2@gmail.com"
+  },
+  {
+    "id": 6,
+    "name": "Isabella",
+    "email": "teste1@gmail.com"
+  },
+  {
+    "id": 7,
+    "name": "Manuela",
+    "email": "teste2@gmail.com"
+  },
+  {
+    "id": 8,
+    "name": "Júlia",
+    "email": "teste1@gmail.com"
+  },
+  {
+    "id": 9,
+    "name": "Heloísa",
+    "email": "teste2@gmail.com"
+  },
+  {
+    "id": 10,
+    "name": "Luiza",
+    "email": "teste1@gmail.com"
+  }
+]
