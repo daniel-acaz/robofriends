6ff2d3d3282c96cf26fc74e140e06482c1a5a916
Daniel <daniel.acaz@becommerce.com.br>
2019-07-20 16:03:26 -0300

    Class 82

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,36 +3,48 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
+import { connect } from 'react-redux';
+import { setSearchField } from '../actions';
 import './App.css';
+
+const mapStateToProps = state => {
+	return { 
+		searchField: state.searchField 
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
 
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: [],
-			searchField: ''
+			robots: []
 		}
 	}
 
 	componentDidMount() {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then(response => response.json())
-			.then(users => this.setState({robots:users}));
-	}
-
-	setOnChante = (event) => {
-		this.setState({searchField: event.target.value})
+			.then(users => {this.setState({robots:users})});
 	}
 
 	render() {
-		const filterRobots = this.state.robots.filter(robot => robot.name.toLowerCase().includes(this.state.searchField.toLowerCase()))
+		const { searchField, onSearchChange } = this.props;
+		const filterRobots = this.state.robots.filter(robot => { 
+				return robot.name.toLowerCase().includes(searchField.toLowerCase()); 
+		})
 		if(this.state.robots.length === 0) {
 			return (<h1>Loading...</h1>)
 		} else {
 			return (
 				<div className = "tc">
 					<h1 className="f1">Robo Friends</h1>
-					<SearchBox setOnChante = {this.setOnChante} />
+					<SearchBox searchChange = {onSearchChange} />
 					<Scroll>
 						<ErrorBoundary>
 							<CardList robots = {filterRobots} />
@@ -44,4 +56,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import App from './containers/App';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
+const store = createStore(searchRobots)
+
 ReactDOM.render(
-	<div>
+	<Provider store = {store}>
 		<App />
-	</div>
-	, document.getElementById('root'));
+	</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
